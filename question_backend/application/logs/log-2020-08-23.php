45ab987551aa2253fb6b11afe2fcfdd7e6acf5a6
dbixxy <dbixxy@gmail.com>
2020-08-24 08:46:18 +0700

    update db

---
+++ question_backend/application/logs/log-2020-08-23.php
@@ -0,0 +1,20 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-08-23 04:23:12 --> Severity: error --> Exception: Call to a member function add() on null D:\xampp\htdocs\cmex_system\question_backend\application\controllers\Home.php 12
+ERROR - 2020-08-23 04:24:19 --> Severity: error --> Exception: Call to a member function add() on null D:\xampp\htdocs\cmex_system\question_backend\application\controllers\Home.php 12
+ERROR - 2020-08-23 04:24:27 --> Severity: error --> Exception: Call to a member function add() on null D:\xampp\htdocs\cmex_system\question_backend\application\controllers\Home.php 12
+ERROR - 2020-08-23 04:25:44 --> 404 Page Not Found: Dist/img
+ERROR - 2020-08-23 04:25:44 --> 404 Page Not Found: Dist/img
+ERROR - 2020-08-23 04:25:44 --> 404 Page Not Found: Dist/img
+ERROR - 2020-08-23 04:25:44 --> 404 Page Not Found: Dist/img
+ERROR - 2020-08-23 04:25:44 --> 404 Page Not Found: Dist/img
+ERROR - 2020-08-23 04:25:44 --> 404 Page Not Found: Dist/img
+ERROR - 2020-08-23 04:25:44 --> 404 Page Not Found: Dist/img
+ERROR - 2020-08-23 04:25:44 --> 404 Page Not Found: Dist/img
+ERROR - 2020-08-23 04:30:14 --> 404 Page Not Found: Dist/img
+ERROR - 2020-08-23 04:30:14 --> 404 Page Not Found: Dist/img
+ERROR - 2020-08-23 04:30:14 --> 404 Page Not Found: Dist/img
+ERROR - 2020-08-23 04:30:14 --> 404 Page Not Found: Dist/img
+ERROR - 2020-08-23 04:30:14 --> 404 Page Not Found: Dist/img
+ERROR - 2020-08-23 04:30:14 --> 404 Page Not Found: Dist/img
+ERROR - 2020-08-23 04:30:14 --> 404 Page Not Found: Dist/img
