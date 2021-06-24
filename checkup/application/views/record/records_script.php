cae8b29c860efc6cb3b5d76edc09e09789ead9c3
dbixxy <dbixxy@gmail.com>
2021-06-24 17:00:47 +0700

    update checkup

--- checkup/application/views/record/records_script.php
+++ checkup/application/views/record/records_script.php
@@ -42,21 +42,86 @@
                 console.log(response);
                 //var res = JSON.parse(response);
                 // alert("Status: " + response.e);
-				lab_arr = JSON.parse(response);
-				txt = "";
-				for(i=0;i<lab_arr.length;i++){
-					if(lab_arr[i].tag=="TEXTAREA"){
-						//alert("TEXTAREA");
-						// txt += lab_arr[i].alt + ": ";
-						for(j=0;j<lab_arr[i].value.length;j++){
-							if(lab_arr[i].value[j]!=""){
-								txt += lab_arr[i].value[j] + "\n";
+				if(response != ""){
+					lab_arr = JSON.parse(response);
+					txt = "";
+					for(i=0;i<lab_arr.length;i++){
+						// if(lab_arr[i].tag=="INPUT"){
+						// 	if (lab_arr[i].type == "radio") {
+						// 		if ((lab_arr[i].checked) && (lab_arr[i].value != "ปกติ (Normal)")) {
+						// 			obj[i]["checked"] = "true";
+						// 			txt += lab_arr[i].placeholder + "\n";
+						// 		}
+						// 	}
+						// }
+						if(lab_arr[i].tag=="TEXTAREA"){
+							
+							for(j=0;j<lab_arr[i].value.length;j++){
+								if(lab_arr[i].value[j]!=""){
+									if(j==0){
+									txt += lab_arr[i].placeholder + ": \n";
+									}
+									txt += lab_arr[i].value[j] + "\n";
+								}
 							}
-						}
-						txt += "\n";
-					}	
+							if((lab_arr[i].value.length > 0) && (lab_arr[i].value[0]!="")){
+								txt += "\n";
+							}
+						}	
+					}
+					$("#exam_lab_detail").val(txt);
 				}
-				$("#exam_lab_detail").val(txt);
+            },
+            error: function (xhr, textStatus, errorThrown) {
+                alert("Status: " + textStatus + '\nError: ' + errorThrown + '\nFunction: load_lab_result');
+            },
+        });
+	}
+
+	function upload_files(checkup_id, tab_id){
+		var ourObj = {};
+		ourObj.checkup_id = checkup_id;
+		ourObj.checkup_id = tab_id;
+        // var points = JSON.stringify(ourObj);
+		var points = ourObj;
+        $.ajax({
+            url:'<?php echo base_url(); ?>Record/serviceUploadFiles/<?php echo $checkup_id; ?>',
+            method: 'POST',
+            data:  points,
+            dataType: 'JSON',
+            success: function(response){
+                console.log(response);
+                //var res = JSON.parse(response);
+                // alert("Status: " + response.e);
+				if(response != ""){
+					lab_arr = JSON.parse(response);
+					txt = "";
+					for(i=0;i<lab_arr.length;i++){
+						// if(lab_arr[i].tag=="INPUT"){
+						// 	if (lab_arr[i].type == "radio") {
+						// 		if ((lab_arr[i].checked) && (lab_arr[i].value != "ปกติ (Normal)")) {
+						// 			obj[i]["checked"] = "true";
+						// 			txt += lab_arr[i].placeholder + "\n";
+						// 		}
+						// 	}
+						// }
+						if(lab_arr[i].tag=="TEXTAREA"){
+							
+							for(j=0;j<lab_arr[i].value.length;j++){
+								if(lab_arr[i].value[j]!=""){
+									if(j==0){
+									txt += lab_arr[i].placeholder + ": \n";
+									}
+									txt += lab_arr[i].value[j] + "\n";
+								}
+							}
+							if((lab_arr[i].value.length > 0) && (lab_arr[i].value[0]!="")){
+								txt += "\n";
+							}
+						}	
+					}
+					$("#exam_lab_detail").val(txt);
+				}
             },
             error: function (xhr, textStatus, errorThrown) {
                 alert("Status: " + textStatus + '\nError: ' + errorThrown + '\nFunction: load_lab_result');
